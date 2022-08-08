93cd23acdb981a976a56ab2f0f4eeabbf5cb313f
MargoSamo <margo_ii@mail.ru>
2022-08-08 15:03:19 +0500

    bubble sort

---
+++ src/main/java/bubble/Bubble.java
@@ -0,0 +1,21 @@
+package bubble;
+public class Bubble {
+    static int[] number = {8, 3, 7, 1, 9, 4};
+
+    public static void main(String[] args) {
+
+        int temp = 0;
+
+        for(int i = 0; i < number.length; i++) {
+            for(int j = i + 1; j < number.length; j++) {
+                if(number[i] > number[j]) {
+                    temp = number[i];
+                    number[i] = number[j];
+                    number[j] = temp;
+                }
+            }
+            System.out.println(number[i]);
+        }
+    }
+}
+
