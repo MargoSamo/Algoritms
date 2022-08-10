507099a451e4cd9457d7a41205dab9129592b34a
MargoSamo <margo_ii@mail.ru>
2022-08-10 17:29:03 +0500

    algoritm min max avg

---
+++ src/main/java/MinMaxAvg/minMaxAvg.java
@@ -0,0 +1,30 @@
+package MinMaxAvg;
+
+public class minMaxAvg {
+
+    public static void main(String[] args) {
+
+        int n = 10;
+        double[] array = new double[n];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Math.random();
+        }
+
+        double max = array[0];
+        double min = array[0];
+        double avg = 0;
+
+        for (int i =0; i < array.length; i++) {
+            if (max < array[i])
+                max = array[i];
+            if (min > array[i])
+                min = array[i];
+            avg += array[i] / array.length;
+        }
+        System.out.println("Max = " + max);
+        System.out.println("Min = " + min);
+        System.out.println("Avg = " + avg);
+
+    }
+
+}
