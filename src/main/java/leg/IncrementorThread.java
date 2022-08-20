57cbe21437b04ebf4ee94fc8394b98c94c609250
MargoSamo <margo_ii@mail.ru>
2022-08-20 10:47:43 +0500

    Thread

--- src/main/java/leg/IncrementorThread.java
+++ src/main/java/leg/IncrementorThread.java
@@ -1,6 +1,18 @@
 package leg;
 
 public class IncrementorThread extends Thread {
+    private final Incrementor incrementor;
+    int i = 0;
 
+    public IncrementorThread(Incrementor incrementor) {
+        this.incrementor = incrementor;
+    }
 
+    @Override
+    public void run() {
+        while (i < 1000) {
+            i++;
+            incrementor.increment();
+        }
+    }
 }
--- src/main/java/leg/Main.java
+++ src/main/java/leg/Main.java
@@ -2,17 +2,21 @@ package leg;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         /*Leg leg = new Leg();
         System.out.println(leg.hasPain());
         leg.injure();
         leg.hasPain();
         System.out.println(leg.hasPain());*/
 
-        new Walk().startWalking();
+        //new Walk().startWalking();
 
         Incrementor incrementor = new Incrementor();
+        IncrementorThread hm = new IncrementorThread(incrementor);
+        IncrementorThread hm1g = new IncrementorThread(incrementor);
 
-
+        hm.join();
+        hm1g.join();
+        System.out.println(incrementor.getValue());
     }
 }
